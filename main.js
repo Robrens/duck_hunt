b2d18eccc46245e40cc880f294137b8ea4582324
Robrens <jbb.baudu@wanadoo.fr>
2018-06-14 16:52:35 +0200

    transition, add a target, repop and score

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 var duck = document.getElementById('root');
+var duck2 = document.getElementById('root2');
 var sky = document.getElementsByTagName('main')
+var scoreText = document.getElementById('score')
 
-function move(){
-window.setInterval(animation, 800);
+var score = 0;
+scoreText.innerHTML = "Votre score est de :" + score;
+
+
+
+function move() {
+    window.setInterval(animation, 800);
 }
 
 function animation() {
-    duck.style.top= Math.random()*90 + "%";
+    duck.style.top = Math.random() * 90 + "%";
+    duck.style.left = Math.random() * 90 + "%";
 
-    duck.style.left = Math.random()*90 + "%"
+    duck2.style.top = Math.random() * 75 + "%";
+    duck2.style.left = Math.random() * 45 + "%";
 
 };
 move()
 
-// while (move) {
-//     animation()
-// }
+duck.style.transition = "0.35s";
+duck2.style.transition = "0.35s";
 
-    
-    
-duck.style.transition= "1s";
+duck.addEventListener("click", function() {
+    score++;
+    scoreText.innerHTML = "Votre score est de :" + score;
+    duck.style.display = "none";
+    setTimeout(function() {
+        duck.style.display = "block"
+    }, 3000);
+});
+
+duck2.addEventListener("click", function() {
+    score++;
+    scoreText.innerHTML = "Votre score est de :" + score;
+    duck2.style.display = "none";
+    setTimeout(function() {
+        duck2.style.display = "block"
+    }, 3000)
+
+
+});
